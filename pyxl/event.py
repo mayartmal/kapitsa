a63ab4d632385bb901e38394ea89b755371765ec
mayartmal <mayartmal@gmail.com>
2018-09-13 20:17:02 +0300

    event

---
+++ pyxl/event.py
@@ -0,0 +1,6 @@
+class Event:
+	def __init__(self, eventWorkOrder, eventTime, eventActualCousingPart):
+	self.eventWorkOrder = eventWorkOrder
+	self.eventTime = eventTime
+	self.eventActualCousingPart = eventActualCousingPart
+			
